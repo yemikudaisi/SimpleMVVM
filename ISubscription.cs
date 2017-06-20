09983bb49bf98df47c30df56de091ad2aae2cc68
Yemi Kudaisi <yemikudaisi@gmail.com>
2017-06-20 15:01:31 +0100

    Added event aggregator components

---
+++ ISubscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMVVM
+{
+    public interface ISubscription<TMessage> : IDisposable
+        where TMessage : IMessage
+    {
+        Action<TMessage> Action { get; }
+        IEventAggregator EventAggregator { get; }
+    }
+
+}
