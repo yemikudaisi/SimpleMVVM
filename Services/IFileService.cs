d2a5161cc00c0b51aa04f6483360b0f0a783fadc
Yemi Kudaisi <yemikudaisi@gmail.com>
2017-06-23 13:10:16 +0100

    Added common service interfaces

---
+++ Services/IFileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slidex.Services
+{
+
+    public interface IFileService
+    {
+        String Path { get; }
+        bool OpenFileDialog(String defaultExt, String filter);
+        bool OpenFileDialog(String defaultExt, String filter, String initDir, String title);
+        bool SaveFileDialog(String defaultExt, String filter);
+        bool SaveFileDialog(String defaultExt, String filter, String initDir, String title);
+        List<String> RecentFiles { get; }
+    }
+}
